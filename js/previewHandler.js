0fe588e13272084d907a77cfec7a6fd5a092087a
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-09-17 14:50:16 +0300

    added html and doc resume

--- js/previewHandler.js
+++ js/previewHandler.js
@@ -1,9 +1,13 @@
 let previewLink = document.querySelector(".preview-info__link");
 previewLink.onclick = function () {
+  let resumeContainer = document.querySelector(".resume-container");
+
   document.querySelector(".preview-info").setAttribute("id", "hingeContainer");
+
   setTimeout(() => {
     document.querySelector(".preview-info").classList.add("hide");
-    document.querySelector(".resume-container").classList.remove("hide");
-    document.querySelector(".resume-container").setAttribute("id", "resumeContainer");
+    resumeContainer.classList.remove("hide");
+    resumeContainer.setAttribute("id", "resumeContainer");
   }, 2000);
+
 };
